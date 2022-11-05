14603a3e30684bc115607872395531bcfb6525c5
PunyaSai182008 <104514490+PunyaSai182008@users.noreply.github.com>
2022-11-05 09:42:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,11 @@ let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
-const changeTurn = () => {
-    return turn === "X" ? "0" : "X"
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
 }
 
-const checkWin = () => {
+const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -21,47 +21,45 @@ const checkWin = () => {
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
     ]
-    wins.forEach(e => {
-        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             document.querySelector(".line").style.width = "20vw";
+            gameover.play();
+
         }
     })
 }
 
 
- 
-music.play()
-music.volume = 0.2;
-
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element => {
+Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', () => {
-        if (boxtext.innerText === '') {
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if (!isgameover) {
-                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-            }
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
         }
     })
 })
 
-reset.addEventListener('click', () => {
+reset.addEventListener('click', ()=>{
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
         element.innerText = ""
     });
-    turn = "X";
+    turn = "X"; 
     isgameover = false
     document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
 
